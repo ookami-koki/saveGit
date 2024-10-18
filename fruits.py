60721aa6b883bf32cbf8c0f527ead6a5c06b9a5f
Koki Iwai <koki4302000@gmail.com>
2024-10-18 10:31:54 +0900

    save .py file

---
+++ fruits.py
@@ -0,0 +1,9 @@
+fruits = ["apple","grape","orange","peach","cherry"]
+
+print("First element:"+ fruits[0])
+print("Second element:"+ fruits[1])
+print("Last element:"+ fruits[-1])
+
+print("０番目から２番目の要素:" , fruits[0:3])
+print("２番めの要素", fruits[2:])
+print("一つ飛ばしでリスト作成", fruits[0::2])
---
+++ var.py
@@ -0,0 +1,4 @@
+name = "岩井"
+money = 95670-95189
+
+print("{}の所持金: {}円" .format(name,money))
